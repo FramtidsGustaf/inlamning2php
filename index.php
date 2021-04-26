4310d60dd91cae837f5dd30170483a8a98a3616c
GustafJ <gustaf89@hotmail.se>
2021-04-26 23:58:43 +0200

    add products

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
 
+include_once "products.php";
+
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header("Referrer-Policy: no-referrer");
 
-for ($i=0; $i < 20; $i++) { 
-  $array[] = array(
-    
-  )
-}
+
+
+
+
+echo json_encode($products, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
