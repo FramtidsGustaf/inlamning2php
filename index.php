a8464f5a72444dce572fa229739f4e6db1e0bc9b
GustafJ <gustaf89@hotmail.se>
2021-04-27 00:55:09 +0200

    wip

--- index.php
+++ index.php
@@ -7,9 +7,45 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header("Referrer-Policy: no-referrer");
+$show = isset($_GET['show']) ? htmlspecialchars($_GET['show']) : 20;
+$category = isset($_GET['category']) ? htmlspecialchars($_GET['category']) : false;
+$array = [];
+
+function validate_category($category)
+{
+  return $category === 'mens clothing' ||
+    $category === 'jewelery' ||
+    $category === 'electronics' ||
+    $category === 'womens clothing';
+}
+
+if (!is_numeric($show) || $show < 1) {
+  http_response_code(400);
+  die('Nonono!');
+}
+
+if ($show < 20) {
+  shuffle($products);
+  for ($i = 0; $i < $show; $i++) {
+    $array[] = $products[$i];
+  }
+}
+
+if ($category) {
+  if (!validate_category($category)) {
+    http_response_code(400);
+    die('Nonono!');
+  }
+  foreach ($products as $product) {
+    if ($product['category'] === $category) {
+      $array[] = $product;
+    }
+  }
+}
+
+if ($show === 20 && !$category) {
+  $array = $products;
+}
 
 
-
-
-
-echo json_encode($products, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+echo json_encode($array, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
