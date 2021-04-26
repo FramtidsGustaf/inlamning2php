1ef1440beb5d5bb9af8a7caa873611a6bfc15a4e
GustafJ <gustaf89@hotmail.se>
2021-04-26 23:10:46 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header("Referrer-Policy: no-referrer");
+
+for ($i=0; $i < 20; $i++) { 
+  $array[] = array(
+    
+  )
+}
