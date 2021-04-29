d8442d9b18248f8673d3b253a1890ace863e3f52
GustafJ <gustaf89@hotmail.se>
2021-04-29 21:55:57 +0200

    comment

--- utils/headers.php
+++ utils/headers.php
@@ -1,5 +1,7 @@
 <?php
 
+//refactored from index.php for cleaner looks
+
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET");
